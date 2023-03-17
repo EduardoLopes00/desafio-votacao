bce2130e21aca6ddc023875d5944a1db6967d65f
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-03-17 01:03:23 -0300

    feat: config database and folder structure

---
+++ src/main/java/com/testdbserver/desafiovotacao/DesafioVotacaoApplication.java
@@ -0,0 +1,12 @@
+package com.testdbserver.desafiovotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVotacaoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testdbserver/desafiovotacao/DesafioVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdbserver.desafiovotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoApplicationTests {
+
+	void contextLoads() {
+
+	}
+
+}
