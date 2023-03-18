cd5bae5cc0ba4072c46dd4300a898f56a33fac90
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-03-18 02:23:11 -0300

    test: Create first tests for associate controller

---
+++ src/test/java/com/testdbserver/desafiovotacao/controllers/AssociateControllerTest.java
@@ -0,0 +1,106 @@
+package com.testdbserver.desafiovotacao.controllers;
+
+import com.testdbserver.desafiovotacao.data.models.Associate;
+import com.testdbserver.desafiovotacao.infra.exceptions.NotFoundException;
+import com.testdbserver.desafiovotacao.services.AssociateService;
+import com.testdbserver.desafiovotacao.utils.TestUtilsFunctions;
+import com.testdbserver.desafiovotacao.utils.mocks.AssociateMocks;
+import com.testdbserver.desafiovotacao.web.DTO.AssociateDTO;
+import com.testdbserver.desafiovotacao.web.controllers.AssociateController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@WebMvcTest(AssociateController.class)
+public class AssociateControllerTest {
+
+    private final String basePath = "/associate";
+
+    @MockBean
+    private AssociateService associateService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturn200_WhenGetAssociateByIdThatExists() throws Exception {
+        when(associateService.getAssociateById(AssociateMocks.DEFAULT_ASSOCIATE_ID)).thenReturn(AssociateMocks.DEFAULT_ASSOCIATE());
+
+        mockMvc.perform(get(basePath + "/{id}", AssociateMocks.DEFAULT_ASSOCIATE_ID))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturn404_WhenGetAssociateByIdThatNotExists() throws Exception {
+        UUID nonexistentAssociateId = UUID.fromString("e12c92e6-c617-464b-9cd1-9c8fbd76a6b6");
+
+        when(associateService.getAssociateById(nonexistentAssociateId)).thenThrow(new NotFoundException(nonexistentAssociateId.toString()));
+
+        mockMvc.perform(get(basePath + "/{id}", nonexistentAssociateId))
+                .andExpect(status().isNotFound()).andExpect(jsonPath("$.message").value("Item not found for ID " + nonexistentAssociateId));
+    }
+
+    @Test
+    public void shouldReturn201_WhenCreateANewAssociate() throws Exception {
+        Associate testingAssociate = AssociateMocks.DEFAULT_ASSOCIATE();
+
+        when(associateService.createAssociate(AssociateDTO.fromModel(testingAssociate))).thenReturn(testingAssociate));
+
+        mockMvc.perform(post(basePath)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtilsFunctions.convertObjectToJSON(AssociateDTO.fromModel(testingAssociate))))
+                .andExpect(status().isCreated()).andExpect(jsonPath("$.id").value(testingAssociate.getId()));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/testdbserver/desafiovotacao/utils/mocks/AssociateMocks.java
+++ src/test/java/com/testdbserver/desafiovotacao/utils/mocks/AssociateMocks.java
@@ -1,4 +1,16 @@
 package com.testdbserver.desafiovotacao.utils.mocks;
 
+import com.testdbserver.desafiovotacao.data.models.Associate;
+import com.testdbserver.desafiovotacao.web.DTO.AssociateDTO;
+
+import java.util.UUID;
+
 public class AssociateMocks {
+
+    public static UUID DEFAULT_ASSOCIATE_ID = UUID.fromString("85a1442d-10f3-4549-93f9-b59a6caf75f5");
+    public static String DEFAULT_ASSOCIATE_CPF = "90473686147";
+
+    public static Associate DEFAULT_ASSOCIATE() {
+        return Associate.builder().id(DEFAULT_ASSOCIATE_ID).cpf(DEFAULT_ASSOCIATE_CPF).email("test222test@gmail.com").build();
+    }
 }
