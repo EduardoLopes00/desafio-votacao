fc613f8f7cbd5163f40928bccb07bcab0acd7479
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-03-19 21:02:55 -0300

    feat: createPauta method

--- src/main/java/com/testdbserver/desafiovotacao/services/PautaService.java
+++ src/main/java/com/testdbserver/desafiovotacao/services/PautaService.java
@@ -4,6 +4,7 @@ import com.testdbserver.desafiovotacao.data.models.Pauta;
 import com.testdbserver.desafiovotacao.data.repositories.PautaRepository;
 import com.testdbserver.desafiovotacao.infra.exceptions.NotFoundException;
 import com.testdbserver.desafiovotacao.services.interfaces.PautaServiceInterface;
+import com.testdbserver.desafiovotacao.web.DTO.PautaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,14 @@ public class PautaService implements PautaServiceInterface {
     @Override
     public Pauta getPautaById(UUID id) {
         return pautaRepository.findById(id).orElseThrow(() -> new NotFoundException(id.toString())) ;
+    }
+
+    @Override
+    public Pauta createPauta(PautaDTO pautaDTO) {
+
+
+        Pauta newPauta = pautaRepository.saveAndFlush(pautaDTO.toModel());
+
+        return newPauta;
     }
 }
--- src/main/java/com/testdbserver/desafiovotacao/services/interfaces/PautaServiceInterface.java
+++ src/main/java/com/testdbserver/desafiovotacao/services/interfaces/PautaServiceInterface.java
@@ -1,9 +1,12 @@
 package com.testdbserver.desafiovotacao.services.interfaces;
 
 import com.testdbserver.desafiovotacao.data.models.Pauta;
+import com.testdbserver.desafiovotacao.web.DTO.PautaDTO;
 
 import java.util.UUID;
 
 public interface PautaServiceInterface {
     public Pauta getPautaById(UUID id);
+
+    public Pauta createPauta(PautaDTO pautaDTO);
 }
