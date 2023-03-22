fc6f66cf8596548ffedd66da5160bdb503a63e58
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-03-22 19:26:56 -0300

    feat: Create requests for searchSection with filters and createSection

--- src/main/java/com/testdbserver/desafiovotacao/web/controllers/SectionController.java
+++ src/main/java/com/testdbserver/desafiovotacao/web/controllers/SectionController.java
@@ -2,12 +2,15 @@ package com.testdbserver.desafiovotacao.web.controllers;
 
 import com.testdbserver.desafiovotacao.data.models.Section;
 import com.testdbserver.desafiovotacao.services.SectionService;
+import com.testdbserver.desafiovotacao.web.DTO.SearchSectionsFiltersDTO;
 import com.testdbserver.desafiovotacao.web.DTO.SectionDTO;
+import com.testdbserver.desafiovotacao.web.DTO.SectionListDTO;
 import com.testdbserver.desafiovotacao.web.controllers.interfaces.SectionControllerInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -30,5 +33,13 @@ public class SectionController implements SectionControllerInterface {
         Section newSection = sectionService.createSection(sectionDTO);
 
         return ResponseEntity.status(201).body(newSection);
+    }
+
+    @Override
+    @PostMapping("/search")
+    public ResponseEntity<List<SectionListDTO>> searchSections(@RequestBody SearchSectionsFiltersDTO searchSectionsFiltersDTO) {
+        List<SectionListDTO> sectionlist = sectionService.searchSections(searchSectionsFiltersDTO);
+
+        return ResponseEntity.status(200).body(sectionlist);
     }
 }
--- src/main/java/com/testdbserver/desafiovotacao/web/controllers/interfaces/SectionControllerInterface.java
+++ src/main/java/com/testdbserver/desafiovotacao/web/controllers/interfaces/SectionControllerInterface.java
@@ -1,16 +1,20 @@
 package com.testdbserver.desafiovotacao.web.controllers.interfaces;
 
-import com.testdbserver.desafiovotacao.data.models.Pauta;
 import com.testdbserver.desafiovotacao.data.models.Section;
-import com.testdbserver.desafiovotacao.web.DTO.PautaDTO;
+import com.testdbserver.desafiovotacao.web.DTO.SearchSectionsFiltersDTO;
 import com.testdbserver.desafiovotacao.web.DTO.SectionDTO;
+import com.testdbserver.desafiovotacao.web.DTO.SectionListDTO;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SectionControllerInterface {
     public ResponseEntity<Section> getById(UUID id);
 
     public ResponseEntity<Section> createSection(SectionDTO sectionDTO);
+
+    public ResponseEntity<List<SectionListDTO>> searchSections(SearchSectionsFiltersDTO searchSectionsFiltersDTO);
+
 
 }
